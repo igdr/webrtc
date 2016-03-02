a5770961bb43381441557091f97966486063fb65
root <drozd.igor@gmail.com>
2016-03-02 10:39:37 +0000

    Remove node_modules

--- typings/browser.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference path="browser/ambient/MediaStream/MediaStream.d.ts" />
-/// <reference path="browser/ambient/RTCPeerConnection/RTCPeerConnection.d.ts" />
-/// <reference path="browser/ambient/peerjs/peerjs.d.ts" />
